4b0da28d7e7a68fe84243806ea578ae159d048ac
Anja Thiart <anjathiart@gmail.com>
2021-11-22 11:40:33 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+	<div id="app">
+		<task-list />
+	</div>
+	</div>
+</template>
+
+<script>
+import TaskList from './components/TaskList.vue';
+
+export default {
+  name: "App",
+  components: {
+	TaskList,
+  }
+};
+</script>
+
+<style>
+@import "./index.css";
+</style>
